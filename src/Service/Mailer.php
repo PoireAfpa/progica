7e1a081a3e12cc73e70ac76dfdc22230f7beabc1
Sttayllah <marielorraine.collignon@gmail.com>
2022-04-19 15:15:53 +0200

    Final push

---
+++ src/Service/Mailer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class Mailer
+{
+
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendEmail()
+    {
+        $email = (new Email())
+            ->from('poire.afpa@gmail.com')
+            ->to('poire.afpa@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Hello world!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>42</p>');
+
+        $this->mailer->send($email);
+
+        // ...
+    }
+}
+
